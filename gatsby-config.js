4af54cd1622a86002a310faa50322cdcb33872a6
shoaib <shoaiblaghari9@gmail.com>
2020-12-29 21:31:58 -0800

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: "dummysite",
+  },
+  plugins: ["gatsby-plugin-styled-components"],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+
+import '../styling/index.css'
+
+const IndexPage = () => {
+  return (
+    <Container class="container">
+      <p class="text">
+        I'll add stuff in a sec
+      </p>
+      <footer>
+        Shoaib Laghari © 2020
+      </footer>
+    </Container>
+  )
+}
+
+export default IndexPage
