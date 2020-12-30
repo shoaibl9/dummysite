e002496e6615365b64fd8fe2d1f6e57cadb9ac7c
shoaib <shoaiblaghari9@gmail.com>
2020-12-30 00:48:35 -0800

    testing link

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,21 @@ import '../styling/index.css'
 const IndexPage = () => {
   return (
     <Container class="container">
-        <p class="text">
-          I'll add stuff in a sec <br />
-          In the meanwhile, <br />
-          check out one of my favorite slam poems: <br />
-          <a
-          class="text-lower"
-          href="https://www.youtube.com/watch?v=vnKZ4pdSU-s"
-          rel="noopener noreferrer"
-          target="_blank">Here</a>
-        </p>
-        <footer>
-          Shoaib Laghari © 2020
-        </footer>
+      <title>Shoaib Laghari</title>
+      <meta charset="utf-8" />
+      <p class="text">
+        I'll add stuff in a sec <br />
+        In the meanwhile, <br />
+        check out one of my favorite slam poems: <br />
+        <a
+        class="text-lower"
+        href="https://www.youtube.com/watch?v=vnKZ4pdSU-s"
+        rel="noopener noreferrer"
+        target="_blank">Here</a>
+      </p>
+      <footer>
+        Shoaib Laghari © 2020
+      </footer>
     </Container>
   )
 }
